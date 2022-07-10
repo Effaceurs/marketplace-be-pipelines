6cbfe36ee28e63239ff91d54726e9b7f333cbccd
Alexander Murylev <effaceurs90@gmail.com>
2022-07-10 19:11:26 +0300

    new pipeline

--- terraform/templates/nginx.tf
+++ terraform/templates/nginx.tf
@@ -1,5 +1,5 @@
 module "$MODULE_NAME" {
-  source = "../../modules/marketplace/$MODULE_NAME"
+  source = "../../../modules/marketplace/$MODULE_NAME"
   namespace = "$NAMESPACE"
   replicas = "$REPLICAS"
   id = "$ID"
