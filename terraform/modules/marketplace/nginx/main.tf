fe4eab8971a7ed2a5cdf75bb7b0edf7a0b189258
Alexander Murylev <effaceurs90@gmail.com>
2022-08-01 22:14:54 +0300

    fix bug

--- terraform/modules/marketplace/nginx/main.tf
+++ terraform/modules/marketplace/nginx/main.tf
@@ -7,11 +7,11 @@ terraform {
 
   backend "kubernetes" {
     config_path      = "/builds/root/marketplace/terraform/config"
-    secret_suffix = ${var.id}
-    namespace = var.namespace
+    secret_suffix = "$ID"
+    namespace = "$NAMESPACE"
   }
 }
 
 provider "kubernetes" {
-  config_path    = "/builds/root/marketplace/terraform/config"
+    config_path    = "/builds/root/marketplace/terraform/config"
 }
--- terraform/modules/marketplace/wordpress/variables.tf
+++ terraform/modules/marketplace/wordpress/variables.tf
@@ -14,3 +14,4 @@ variable "id" {
 variable "appVersion" {
   type = string
 }
+
